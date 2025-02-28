6bc4982fe76fb5ba908365596110659cda718852
victorcogliolo <vrcogliolo@gmail.com>
2025-02-28 09:35:24 -0300

    added environment variable

--- src/app/services/subscription.service.ts
+++ src/app/services/subscription.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class SubscriptionService {
-  private apiUrl = 'http://localhost:8000/suscripcion';  
+  private apiUrl = environment.BACKEND;
   constructor(private http: HttpClient) {}
 
   subscribe(email: string): Observable<any> {
