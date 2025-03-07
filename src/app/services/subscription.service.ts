65ebc3a1c61ebb2c6f8482dce76114aa83b3ce25
victorcogliolo <vrcogliolo@gmail.com>
2025-03-07 12:36:51 -0300

    fix

--- src/app/services/subscription.service.ts
+++ src/app/services/subscription.service.ts
@@ -2,20 +2,17 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
-export class SuscripcionService {
-  private apiUrl = 'http://localhost:8000/suscripcion';  
+export class SubscriptionService {
+  private apiUrl = environment.BACKEND;  
 
   constructor(private http: HttpClient) {}
 
   suscribirse(email: string): Observable<any> {
-    return this.http.post<any>(this.apiUrl, { email }).pipe(
-      catchError(error => {
-        return throwError(() => new Error(error.error?.detail || "Hubo un error al suscribirse."));
-      })
-    );
+    return this.http.post<any>(`${this.apiUrl}/newsletter`, { email });
   }
 }
