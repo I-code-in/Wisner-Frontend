fd38b53282bc66e4570de2b0bd4b203b3c45289b
victorcogliolo <vrcogliolo@gmail.com>
2025-03-14 18:19:28 -0300

    Fix

--- src/app/services/banner-images.service.ts
+++ src/app/services/banner-images.service.ts
@@ -1,17 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BannerImagesService {
-  private apiUrl = 'http://localhost:8000/banner_images/active'; // Ajusta la URL según tu backend
+  private apiUrl = environment.BACKEND;
 
   constructor(private http: HttpClient) {}
 
 
   getBannerImages(): Observable<any[]> {
-    return this.http.get<any[]>(this.apiUrl);
+    return this.http.get<any[]>(`${this.apiUrl}/banner_images/active`);
   }
 }
